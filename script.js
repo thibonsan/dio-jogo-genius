ac6bd3672a928286c3b347f10d693378df1f52b1
Thiago Bonetti dos Santos <thibonsan@gmail.com>
2022-12-15 10:59:29 +0000

    Parte 5.

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ let lightColor = (element, number) => {
 let checkOrder = () => {
     for(let i in clickedOrder) {
         if (clickedOrder[i] != order[i]) {
-            localStorage();
+            gameOver();
             break;
         }
     }
@@ -55,7 +55,43 @@ let click = (color) => {
 
     setTimeout(() => {
         createColorElement(color).classList.remove('selected');
-    });
-
-    checkOrder();
+        checkOrder();
+    }, 250);
 }
+
+let createColorElement = (color) => {
+    if (color == 0) {
+        return green;
+    } else if (color == 1) {
+        return red;
+    }else if (color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
+
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+let gameOver = () => {
+    alert(`Pontuação: ${score}\nVocê perdeu o jogo\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+    playGame();
+}
+
+let playGame = () => {
+    alert(`Bem vindo ao Genius! Iniciando um novo jogo!`)
+    score = 0;
+    nextLevel();
+}
+
+green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));
+
+playGame();
